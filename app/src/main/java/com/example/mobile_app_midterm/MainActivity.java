23be24ed89976964e3d97b1f7791a90854a9ad08
eric-laudrum <ericlaudrum@gmail.com>
2025-10-30 17:55:24 -0400

    Fix table generation and add delete button

--- app/src/main/java/com/example/mobile_app_midterm/MainActivity.java
+++ app/src/main/java/com/example/mobile_app_midterm/MainActivity.java
@@ -1,22 +1,23 @@
 package com.example.mobile_app_midterm;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText enterNumber;
-    private ListView listView;
+    private LinearLayout multiplicationTable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +30,22 @@ public class MainActivity extends AppCompatActivity {
                     v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
                     return insets;
                 });
+
+        // Initialize values from xml
         enterNumber = findViewById(R.id.enterNumber);
-        listView = findViewById(R.id.listView);
+        multiplicationTable = findViewById(R.id.multiplicationTable);
+
+        // Set enter button to create table
+        Button enterButton = findViewById(R.id.enterNumberButton);
+        enterButton.setOnClickListener(v->createTable());
         }
 
     // Create table
-    public void createTable(View view){
+    public void createTable(){
+
+        // Clear table of all values / views
+        multiplicationTable.removeAllViews();
+
         // Get the entered number
         String input = enterNumber.getText().toString();
 
@@ -46,25 +57,34 @@ public class MainActivity extends AppCompatActivity {
 
         // Process input
         int number = Integer.parseInt(input);
-        Toast.makeText(this, "Number entered: " + number, Toast.LENGTH_SHORT).show();
+       // Toast.makeText(this, "Number entered: " + number, Toast.LENGTH_SHORT).show();
 
-        String[] multiplicationTable = new String[number -1];
-        for(int i = 0; i < number; i++) {
-            // Create string value for table
-            multiplicationTable[i] = "x" + i + " = " + (number * i);
+        for(int i = 0; i <= 10; i++) {
+            // Create string equation for table
+            String rowText = number + " x " + i + " = " + (number * i);
 
+            // Create new row
+            LinearLayout row = new LinearLayout(this);
+
+            TextView textView = new TextView(this);
+            textView.setText(rowText);
+
+            // Add delete button
+            Button deleteButton = new Button(this);
+            deleteButton.setText("x");
+
+            // Delete row if clicked
+            deleteButton.setOnClickListener(v-> {
+                multiplicationTable.removeView(row);
+                Toast.makeText(this, "Deleted " + rowText, Toast.LENGTH_SHORT).show();
+            });
+
+            // Adjust layout
+            row.setOrientation(LinearLayout.HORIZONTAL);
+            row.addView(textView);
+            row.addView(deleteButton);
+
+            multiplicationTable.addView(row);
         }
-
-        // Show in table:
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                multiplicationTable
-        );
-        listView.setAdapter(adapter);
     }
-
-    }
-
-
 }
